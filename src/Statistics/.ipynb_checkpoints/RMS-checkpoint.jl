4d2d978bf144d2f827209dda254d829512a913a3
Jack Berat <jack.berat@laposte.net>
2026-04-22 13:26:16 +0200

    Remove notebook checkpoint files

--- src/Statistics/.ipynb_checkpoints/RMS-checkpoint.jl
+++
@@ -1,3 +0,0 @@
-RMS(X,Y,Z) = sqrt((vec(X).-mean(vec(X)))^2+(vec(Y).-mean(vec(Y)))^2+(vec(Z).-mean(vec(Z)))^2)
-RMS(X,Y) = sqrt((vec(X).-mean(vec(X)))^2+(vec(Y).-mean(vec(Y)))^2)
-RMS(X) = sqrt(vec(X)-mean(vec(X).^2))
